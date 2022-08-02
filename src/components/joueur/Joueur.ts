74ecd709e3df697ea53d110f21d4cdc7669eeb14
Adeline64 <adeline.lacabanne@gmail.com>
2022-08-02 15:33:24 +0200

    ajout getter et setter class personnage

---
+++ src/components/joueur/Joueur.ts
@@ -0,0 +1,8 @@
+import { Personnage } from "../personnage/Personnage";
+
+export class Joueur extends Personnage{
+
+  demanderCarte(){
+    
+  }
+}
--- src/components/personnage/Personnage.ts
+++ src/components/personnage/Personnage.ts
@@ -1,9 +1,25 @@
 export class Personnage {
-    private cartes: number[] = []
-    private score : number = 0
+    private _cartes: number[] = []
+    private _score : number = 0
     
     constructor() {
 
+    }
+
+    get cartes() {
+      return this._cartes;
+    }
+
+    set cartes(cartes: number[]){
+      this.cartes  = cartes;
+    }
+
+    get score() {
+      return this._score
+    }
+
+    set score(score:number){
+      this._score = score;
     }
 
     gagnant(){
